b36a441fe9f5f3c247bd109167c7a65e41f263f0
Vitor Franco <vitoroliveirafranco@gmail.com>
2022-09-26 09:19:10 -0300

    chore: fix expiration time JWT Token to 10min

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,7 +1,7 @@
 export default {
   token: {
     secret: '0f5acfcfe0435048f619acd5f433e808',
-    expires: '30d' // TODO: Change to 10m
+    expires: '10m'
   },
   refreshToken: {
     secret: '123c09307230d6dfb5f5faff5acabeae',
